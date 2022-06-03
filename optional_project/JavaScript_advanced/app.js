62cb16ebbeb7bdd0b7ffdcb0f57c4a18fa341fe4
young jo <a10019610@gmail.com>
2022-06-04 01:05:00 +0900

    [Day-31~33] JavaScript 기초와 고급 JavaScript DOM 개념

---
+++ optional_project/JavaScript_advanced/app.js
@@ -0,0 +1,6 @@
+console.log(window);
+
+alert();
+window.alert();
+
+console.log(document);
--- optional_project/Javascript_Intro/operations.js
+++ optional_project/Javascript_Intro/operations.js
@@ -3,3 +3,16 @@ console.log(10 * 2);
 console.log(10 - 10);
 console.log(10 / 10);
 console.log(10 % 4);
+console.log(10 % 3);
+
+console.log((10 + 3 - 5) * 10);
+
+console.log("Yang" + "YoungJO");
+console.log("Yang" - "ng");
+
+let username = "Yang";
+console.log(username.length);
+console.log(username.toUpperCase);
+
+let hobbies = ["cooking", "baseball"];
+console.log(hobbies.length);
