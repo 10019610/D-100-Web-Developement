94c86010a4c45ba2682ca61fd059486a1e19e583
young jo <a10019610@gmail.com>
2022-07-05 22:32:16 +0900

    [Day-70~73, 81] 사용자 인증, 세션, 온라인상점 세션

--- final_project/routes/auth.routes.js
+++ final_project/routes/auth.routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const db = require('../data/database')
+
 const authController = require("../controllers/auth.controller");
 
 const router = express.Router();
--- session/data/database.js
+++ session/data/database.js
@@ -1,32 +1,33 @@
 // const mongodb = require('mongodb');
-// const { get } = require('../routes/auth.routes');
 
 // const MongoClient = mongodb.MongoClient;
 
 // let database;
 
 // async function connectToDatabase() {
-// 	const client = await MongoClient.connect('mongodb://localhost:27017');
-// 	database = client.db('online-shop');
+//   const client = await MongoClient.connect(
+//     'mongodb://localhost:27017'
+//   );
+//   database = client.db('auth-demo');
 // }
 
 // function getDb() {
-// 	if (!database) {
-// 		throw new Error('You must connect first!!!');
-// 	}
-// 	return database;
+//   if (!database) {
+//     throw { message: 'You must connect first!' };
+//   }
+//   return database;
 // }
 
 // module.exports = {
-// 	connectToDatabase: connectToDatabase,
-// 	getDb: getDb
+//   connectToDatabase: connectToDatabase,
+//   getDb: getDb,
 // };
 
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
 	host: "localhost",
-	database: "final",
+	database: "session",
 	user: "root",
 	password: "1234",
 });
