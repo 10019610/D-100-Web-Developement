ffa88df99c1f7e62b7a67300a51cefdd99dbce2a
young jo <a10019610@gmail.com>
2022-07-11 16:17:11 +0900

    [D-74~78] 보안, 인증, 리팩토링, MVC 패턴
    지난주 금,일에 실습한 내용 commit

--- session/app.js
+++ session/app.js
@@ -11,11 +11,11 @@ const demoRoutes = require('./routes/demo');
 
 const app = express();
 
-// const sessionStore = new MongoDBStore({
-// 	uri: 'mongodb://localhost:27017',
-// 	databaseName: 'auth-demo',
-// 	collection: 'sessions'
-// });
+const sessionStore = new MongoDBStore({
+	url: 'mongodb://localhost:27017',
+	databaseName: 'auth-demo',
+	collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -23,15 +23,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
-// app.use(session({
-// 	secret: 'super-secret',
-// 	resave: false,
-// 	saveUninitialized: false,
-// 	store: sessionStore,
-// 	cookie: {
-// 		maxAge: 2 * 24 * 60 * 60 * 1000
-// 	}
-// }));
+app.use(session({
+	secret: 'super-secret',
+	resave: false,
+	saveUninitialized: false,
+	store: sessionStore,
+	cookie: {
+		maxAge: 2 * 24 * 60 * 60 * 1000
+	}
+}));
 
 app.use(async function (req, res, next) {
 	const user = req.session.user;
